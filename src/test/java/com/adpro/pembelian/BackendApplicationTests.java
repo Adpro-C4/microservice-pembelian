ab3ae81cb90af7de2957b5af7c4b83bda2fb294d
isaui <isadestroyed17@gmail.com>
2024-04-17 10:17:12 +0700

    init: init  microservice pembelian

---
+++ src/test/java/com/adpro/pembelian/BackendApplicationTests.java
@@ -0,0 +1,29 @@
+package com.adpro.pembelian;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.dadcompfest.backend.BackendApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Autowired
+    private BackendApplication backendApplication;
+
+
+    @Test
+    void contextLoads() {
+        assertNotNull(backendApplication);
+    }
+
+    @Test
+    void testSpringApplicationRun() {
+         assertDoesNotThrow(() -> backendApplication.main(new String[] {}));
+
+    }
+
+}
