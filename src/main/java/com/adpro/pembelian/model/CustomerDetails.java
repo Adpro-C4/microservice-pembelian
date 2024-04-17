9ec1a062be43f9c9630f961a471086bc50069d56
isaui <isadestroyed17@gmail.com>
2024-04-17 18:39:05 +0700

    GREEN: impl customer details dto

---
+++ src/main/java/com/adpro/pembelian/model/CustomerDetails.java
@@ -0,0 +1,14 @@
+package com.adpro.pembelian.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerDetails {
+    private String fullname;
+    private String username;
+    private String userId;
+    private String phoneNumber;
+    private String email;
+}
