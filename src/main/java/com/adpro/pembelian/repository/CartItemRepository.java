d975fe5b8aa61534fa451b861d1f46f5011a1145
isaui <isadestroyed17@gmail.com>
2024-04-17 20:49:48 +0700

    feat<repository>:implement jparepository

--- src/main/java/com/adpro/pembelian/repository/CartItemRepository.java
+++ src/main/java/com/adpro/pembelian/repository/CartItemRepository.java
@@ -1,4 +1,7 @@
 package com.adpro.pembelian.repository;
 
-public interface CartItemRepository {
+import com.adpro.pembelian.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
--- src/main/java/com/adpro/pembelian/repository/ShoppingCartRepository.java
+++ src/main/java/com/adpro/pembelian/repository/ShoppingCartRepository.java
@@ -1,4 +1,7 @@
 package com.adpro.pembelian.repository;
 
-public interface ShoppingCartRepository {
+import com.adpro.pembelian.model.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
 }
