335611c9fdae3258e32808693beaa99318b18854
isaui <isadestroyed17@gmail.com>
2024-04-17 17:12:32 +0700

    GREEN: add voucherbuilder

---
+++ src/main/java/com/adpro/pembelian/model/VoucherBuilder.java
@@ -0,0 +1,51 @@
+package com.adpro.pembelian.model;
+
+public class VoucherBuilder {
+    private Long voucherId;
+    private String voucherName;
+    private String voucherDescription;
+    private double voucherDiscount;
+    private String voucherType;
+    private Integer voucherQuota;
+
+    public VoucherBuilder voucherId(Long voucherId) {
+        this.voucherId = voucherId;
+        return this;
+    }
+
+    public VoucherBuilder voucherName(String voucherName) {
+        this.voucherName = voucherName;
+        return this;
+    }
+
+    public VoucherBuilder voucherDescription(String voucherDescription) {
+        this.voucherDescription = voucherDescription;
+        return this;
+    }
+
+    public VoucherBuilder voucherDiscount(double voucherDiscount) {
+        this.voucherDiscount = voucherDiscount;
+        return this;
+    }
+
+    public VoucherBuilder voucherType(String voucherType) {
+        this.voucherType = voucherType;
+        return this;
+    }
+
+    public VoucherBuilder voucherQuota(Integer voucherQuota) {
+        this.voucherQuota = voucherQuota;
+        return this;
+    }
+
+    public Voucher build() {
+        Voucher voucher = new Voucher();
+        voucher.setVoucherId(this.voucherId);
+        voucher.setVoucherName(this.voucherName);
+        voucher.setVoucherDescription(this.voucherDescription);
+        voucher.setVoucherDiscount(this.voucherDiscount);
+        voucher.setVoucherType(this.voucherType);
+        voucher.setVoucherQuota(this.voucherQuota);
+        return voucher;
+    }
+}
