451139ba51bb97c9fa3d5c9e00543f7658a6b90d
isaui <isadestroyed17@gmail.com>
2024-04-23 19:41:23 +0700

    RED: Create test for CustomerDetailsService

--- src/main/java/com/adpro/pembelian/service/UserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.adpro.pembelian.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserDetailsService {
-    UserDetails getUserDetails(Long userId);
-}
---
+++ src/test/java/com/adpro/pembelian/service/CustomerDetailsServiceTest.java
@@ -0,0 +1,2 @@
+package com.adpro.pembelian.service;public class CustomerDetailsServiceTest {
+}
