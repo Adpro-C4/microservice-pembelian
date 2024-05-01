6ede0346e954fbdeaf897873486b38c322cbf748
isaui <isadestroyed17@gmail.com>
2024-05-01 16:14:43 +0700

    REFACTOR: fix entity for order, make decorator pattern into logic

--- src/main/java/com/adpro/pembelian/model/entity/OrderWithVoucherEntity.java
+++ src/main/java/com/adpro/pembelian/model/entity/OrderWithVoucherEntity.java
@@ -3,14 +3,25 @@ package com.adpro.pembelian.model.entity;
 import com.adpro.pembelian.model.decorator.OrderWithVoucherDecorator;
 import com.adpro.pembelian.model.dto.DTOVoucher;
 import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class OrderWithVoucherEntity extends OrderTemplate{
     @Embedded
     DTOVoucher voucher;
-    OrderWithVoucherEntity(){};
+
     OrderWithVoucherEntity(DTOVoucher voucher){
         this.voucher = voucher;
     }
+
+    public OrderWithVoucherEntity() {
+
+    }
+
     @Override
     public double getTotalPrice() {
         return 0;
