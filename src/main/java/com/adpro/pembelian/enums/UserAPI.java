06ba140c0d56431c75040f2ae3070e398991fb42
isaui <isadestroyed17@gmail.com>
2024-05-07 19:58:58 +0700

    feat: connect user api via gateway

--- src/main/java/com/adpro/pembelian/enums/UserAPI.java
+++ src/main/java/com/adpro/pembelian/enums/UserAPI.java
@@ -2,7 +2,7 @@ package com.adpro.pembelian.enums;
 
 public enum UserAPI {
 
-    GET_USERDATA("https://specialitystorebackend.up.railway.app/data/customer");
+    GET_USERDATA("https://api-gateway-specialitystore.up.railway.app/user-data-service/data/customer");
     private final   String url;
     UserAPI(String url){
         this.url = url;
