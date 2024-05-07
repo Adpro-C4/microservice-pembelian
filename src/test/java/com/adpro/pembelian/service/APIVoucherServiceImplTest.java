1467c1e07c2b39371824992b342bff9620ef2aaf
isaui <isadestroyed17@gmail.com>
2024-05-07 21:23:02 +0700

    feat: create async task executor and configuration for springboot to enable async

--- src/test/java/com/adpro/pembelian/service/APIVoucherServiceImplTest.java
+++ src/test/java/com/adpro/pembelian/service/APIVoucherServiceImplTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-import java.util.NoSuchElementException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
