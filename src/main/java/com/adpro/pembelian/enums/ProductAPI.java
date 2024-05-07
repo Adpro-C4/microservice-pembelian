fca891e699a639d5e5f350fab081ebd090a91df8
isaui <isadestroyed17@gmail.com>
2024-05-07 19:56:44 +0700

    feat: connect product api via gateway

--- src/main/java/com/adpro/pembelian/enums/ProductAPI.java
+++ src/main/java/com/adpro/pembelian/enums/ProductAPI.java
@@ -1,8 +1,8 @@
 package com.adpro.pembelian.enums;
 
 public enum ProductAPI {
-    ALL_PRODUCT("https://microservice-katalog-production.up.railway.app/product/all"),
-    PRODUCT("https://microservice-katalog-production.up.railway.app/product");
+    ALL_PRODUCT("https://api-gateway-specialitystore.up.railway.app/product-service/product/all"),
+    PRODUCT("https://api-gateway-specialitystore.up.railway.app/product-service/product");
     private final   String url;
     ProductAPI(String url){
         this.url = url;
